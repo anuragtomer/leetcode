15dc7c3cd5036a9205bdae14c0c45c39e91655b3
Anurag Tomer <anuragtomer12@gmail.com>
2022-07-17 07:51:07 +0530

    Questions 17/07/2022

--- 435.non-overlapping-intervals.cpp
+++ 435.non-overlapping-intervals.cpp
@@ -5,34 +5,33 @@
 using namespace std;
 
 class Solution {
-   public:
-    int eraseOverlapIntervals(vector<vector<int>> &intervals) {
-        sort(intervals.begin(), intervals.end());
-        int last = 0, count = 0;
-        for (int i = 1, n = intervals.size(); i < n; ++i) {
-            if (intervals[i][0] >= intervals[last][1]) {
-                // All good.
-                last = i;
-            } else {
-                ++count;
-                // I have to discard one of them. Discard which finishes
-                // first so that I can accomodate more intervals.
-                if (intervals[i][1] < intervals[last][1]) {
-                    // discarding previous interval
-                    last = i;
-                } else {
-                    // Nothing to do here. last stays as is. We have discarded i'th interval.
-                    ;
-                }
-            }
+ public:
+  int eraseOverlapIntervals(vector<vector<int>> &intervals) {
+    sort(intervals.begin(), intervals.end());
+    int last = 0, count = 0;
+    for (int i = 1, n = intervals.size(); i < n; ++i) {
+      if (intervals[i][0] >= intervals[last][1]) {
+        // All good.
+        last = i;
+      } else {
+        ++count;
+        // I have to discard one of them. Discard which finishes
+        // first so that I can accomodate more intervals.
+        if (intervals[i][1] < intervals[last][1]) {
+          // discarding previous interval
+          last = i;
+        } else {
+          // Nothing to do here. last stays as is. We have discarded i'th interval.
+          ;
         }
-        return count;
+      }
     }
+    return count;
+  }
 };
 
 int main() {
-    Solution sol;
+  Solution sol;
 
-    return 0;
+  return 0;
 }
-
