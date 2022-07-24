8ba3ce1f43e6fdabe3750bc633cd01797bcc24d4
Anurag Tomer <anuragtomer12@gmail.com>
2022-07-24 19:57:52 +0530

    Questions 24/07/2022

--- 417.pacific-atlantic-water-flow.cpp
+++ 417.pacific-atlantic-water-flow.cpp
@@ -75,58 +75,63 @@ class Solution {
 */
 // Another bfs/dfs based solution using 2 matrices, one for atlantic, another for pacific.
 class Solution {
-    void bfs(queue<pair<int, int>> &q, vector<vector<bool>> &visited, vector<vector<int>> &nums, int m, int n) {
-        vector<vector<int>> direction = {{1, 0}, {-1, 0}, {0, -1}, {0, 1}};
-        while (!q.empty()) {
-            int i = q.front().first, j = q.front().second;
-            q.pop();
-            for (auto dir : direction) {
-                auto x = i + dir[0], y = j + dir[1];
-                if (x < 0 || x >= m || y < 0 || y >= n || visited[x][y])
-                    continue;
-                if (nums[i][j] <= nums[x][y]) {
-                    visited[x][y] = true;
-                    q.push({x, y});
-                }
-            }
+  void bfs(queue<pair<int, int>> &q, vector<vector<bool>> &visited,
+           vector<vector<int>> &nums, int m, int n) {
+    vector<vector<int>> direction = {{1, 0}, {-1, 0}, {0, -1}, {0, 1}};
+    while (!q.empty()) {
+      int i = q.front().first, j = q.front().second;
+      q.pop();
+      for (auto dir : direction) {
+        auto x = i + dir[0], y = j + dir[1];
+        if (x < 0 || x >= m || y < 0 || y >= n || visited[x][y])
+          continue;
+        if (nums[i][j] <= nums[x][y]) {
+          visited[x][y] = true;
+          q.push({x, y});
         }
+      }
     }
+  }
 
-   public:
-    vector<vector<int>> pacificAtlantic(vector<vector<int>> &heights) {
-        vector<vector<int>> result;
-        int m = heights.size(), n = heights[0].size();
-        vector<vector<bool>> atlantic(m, vector<bool>(n, false)), pacific(m, vector<bool>(n, false));
-        queue<pair<int, int>> atlanticQueue, pacificQueue;
-        for (int i = 0; i < m; ++i) {
-            pacific[i][0] = true;
-            atlantic[i][n - 1] = true;
-            pacificQueue.push({i, 0});
-            atlanticQueue.push({i, n - 1});
-        }
-        for (int i = 0; i < n; ++i) {
-            pacific[0][i] = true;
-            atlantic[m - 1][i] = true;
-            pacificQueue.push({0, i});
-            atlanticQueue.push({m - 1, i});
-        }
-        bfs(pacificQueue, pacific, heights, m, n);
-        bfs(atlanticQueue, atlantic, heights, m, n);
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (pacific[i][j] && atlantic[i][j])
-                    result.push_back({i, j});
-            }
-        }
-        return result;
+ public:
+  vector<vector<int>> pacificAtlantic(vector<vector<int>> &heights) {
+    vector<vector<int>> result;
+    int m = heights.size(), n = heights[0].size();
+    vector<vector<bool>> atlantic(m, vector<bool>(n, false)),
+      pacific(m, vector<bool>(n, false));
+    queue<pair<int, int>> atlanticQueue, pacificQueue;
+    for (int i = 0; i < m; ++i) {
+      pacific[i][0] = true;
+      atlantic[i][n - 1] = true;
+      pacificQueue.push({i, 0});
+      atlanticQueue.push({i, n - 1});
     }
+    for (int i = 0; i < n; ++i) {
+      pacific[0][i] = true;
+      atlantic[m - 1][i] = true;
+      pacificQueue.push({0, i});
+      atlanticQueue.push({m - 1, i});
+    }
+    bfs(pacificQueue, pacific, heights, m, n);
+    bfs(atlanticQueue, atlantic, heights, m, n);
+    for (int i = 0; i < m; ++i) {
+      for (int j = 0; j < n; ++j) {
+        if (pacific[i][j] && atlantic[i][j])
+          result.push_back({i, j});
+      }
+    }
+    return result;
+  }
 };
 int main() {
-    Solution sol;
-    vector<vector<int>> heights = {{1, 2, 2, 3, 5}, {3, 2, 3, 4, 4}, {2, 4, 5, 3, 1}, {6, 7, 1, 4, 5}, {5, 1, 1, 2, 4}};
-    vector<vector<int>> result = sol.pacificAtlantic(heights);
-    for (auto res : result)
-        cout << res[0] << " " << res[1] << endl;
-    return 0;
+  Solution sol;
+  vector<vector<int>> heights = {{1, 2, 2, 3, 5},
+                                 {3, 2, 3, 4, 4},
+                                 {2, 4, 5, 3, 1},
+                                 {6, 7, 1, 4, 5},
+                                 {5, 1, 1, 2, 4}};
+  vector<vector<int>> result = sol.pacificAtlantic(heights);
+  for (auto res : result)
+    cout << res[0] << " " << res[1] << endl;
+  return 0;
 }
-
--- 994.rotting-oranges.cpp
+++ 994.rotting-oranges.cpp
@@ -6,43 +6,43 @@
 using namespace std;
 
 class Solution {
-   public:
-    int orangesRotting(vector<vector<int>> &grid) {
-        queue<pair<int, int>> bfs;
-        for (int i = 0; i < grid.size(); ++i)
-            for (int j = 0; j < grid[0].size(); ++j)
-                if (grid[i][j] == 2)
-                    bfs.push({i, j});
-        int time = 0;
-        vector<vector<int>> direction = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-        while (!bfs.empty()) {
-            int len = bfs.size();
-            while (len) {
-                --len;
-                auto [i, j] = bfs.front();
-                bfs.pop();
-                grid[i][j] = 2;
-                for (auto dir : direction) {
-                    int x = i + dir[0], y = j + dir[1];
-                    if (x < 0 || x >= grid.size() || y < 0 || y >= grid[x].size() || grid[x][y] != 1)
-                        continue;
-                    grid[x][y] = 2;
-                    bfs.push({x, y});
-                }
-            }
-            ++time;
+ public:
+  int orangesRotting(vector<vector<int>> &grid) {
+    queue<pair<int, int>> bfs;
+    for (int i = 0; i < grid.size(); ++i)
+      for (int j = 0; j < grid[0].size(); ++j)
+        if (grid[i][j] == 2)
+          bfs.push({i, j});
+    int time = 0;
+    vector<vector<int>> direction = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    while (!bfs.empty()) {
+      int len = bfs.size();
+      while (len) {
+        --len;
+        auto [i, j] = bfs.front();
+        bfs.pop();
+        grid[i][j] = 2;
+        for (auto dir : direction) {
+          int x = i + dir[0], y = j + dir[1];
+          if (x < 0 || x >= grid.size() || y < 0 || y >= grid[x].size() ||
+              grid[x][y] != 1)
+            continue;
+          grid[x][y] = 2;
+          bfs.push({x, y});
         }
-        for (int i = 0; i < grid.size(); ++i)
-            for (int j = 0; j < grid[0].size(); ++j)
-                if (grid[i][j] == 1)
-                    return -1;
-        return max(0, time - 1);
+      }
+      ++time;
     }
+    for (int i = 0; i < grid.size(); ++i)
+      for (int j = 0; j < grid[0].size(); ++j)
+        if (grid[i][j] == 1)
+          return -1;
+    return max(0, time - 1);
+  }
 };
 
 int main() {
-    Solution sol;
+  Solution sol;
 
-    return 0;
+  return 0;
 }
-
