d209b4983507e961f2af16f26e834b8c692888db
Anurag Tomer <anuragtomer12@gmail.com>
2022-07-27 07:08:12 +0530

    Questions 27/07/2022

--- 114.flatten-binary-tree-to-linked-list.cpp
+++ 114.flatten-binary-tree-to-linked-list.cpp
@@ -6,42 +6,42 @@
 using namespace std;
 
 struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+    : val(x), left(left), right(right) {}
 };
 
 class Solution {
-   public:
-    void flatten(TreeNode *root) {
-        if (!root)
-            return;
-        stack<TreeNode *> st;
-        st.push(root);
-        TreeNode *temp = nullptr, *last = nullptr;
-        while (st.empty() == false || temp != nullptr) {
-            if (temp == nullptr) {
-                temp = st.top();
-                st.pop();
-                if (last)
-                    last->right = temp;
-            }
-            if (temp->right)
-                st.push(temp->right);
-            temp->right = temp->left;
-            temp->left = nullptr;
-            last = temp;
-            temp = temp->right;
-        }
+ public:
+  void flatten(TreeNode *root) {
+    if (!root)
+      return;
+    stack<TreeNode *> st;
+    st.push(root);
+    TreeNode *temp = nullptr, *last = nullptr;
+    while (st.empty() == false || temp != nullptr) {
+      if (temp == nullptr) {
+        temp = st.top();
+        st.pop();
+        if (last)
+          last->right = temp;
+      }
+      if (temp->right)
+        st.push(temp->right);
+      temp->right = temp->left;
+      temp->left = nullptr;
+      last = temp;
+      temp = temp->right;
     }
+  }
 };
 
 int main() {
-    Solution sol;
+  Solution sol;
 
-    return 0;
+  return 0;
 }
-
