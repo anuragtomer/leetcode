150d0e8e6e0e254757bcf122facc04c73fa74fb0
Anurag Tomer <anuragtomer12@gmail.com>
2022-08-06 07:35:17 +0530

    Questions 06/08/2022

---
+++ 458.poor-pigs.cpp
@@ -0,0 +1,10 @@
+class Solution {
+ public:
+  int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+    int T = minutesToTest / minutesToDie;
+    int x = 0;
+    while ((pow(T + 1), x) < buckets)
+      ++x;
+    return x;
+  }
+}
--- 528.random-pick-with-weight.cpp
+++ 528.random-pick-with-weight.cpp
@@ -5,26 +5,27 @@
 using namespace std;
 
 class Solution {
-    vector<float> probs;
+  vector<float> probs;
+  random_device rd;
+  uniform_float_distribution<float> dist;
+  mt19937 mt;
 
-   public:
-    Solution(vector<int> &w) {
-        float sum = accumulate(w.begin(), w.end(), 0.0);
-        probs.reserve(w.size());
-        float prob = 0;
-        for (auto i : w) {
-            prob += ((float)i / sum); // prob += ((float) i * 100 / sum);
-            probs.push_back(prob);
-        }
+ public:
+  Solution(vector<int> &w) {
+    float sum = accumulate(w.begin(), w.end(), 0.0);
+    probs.reserve(w.size());
+    float prob = 0;
+    for (auto i : w) {
+      prob += (i / sum); // prob += ((float) i * 100 / sum);
+      probs.push_back(prob);
     }
-    int pickIndex() {
-        random_device rd;
-        mt19937 mt(rd());
-        uniform_real_distribution<float> dist(0.0, 1.0);
-        float pick = dist(mt);
-        auto it = upper_bound(probs.begin(), probs.end(), pick);
-        return it - probs.begin();
-        /*
+    mt = mt19937(rd());
+    dist = uniform_float_distribution<float>(0.0, 1.0);
+  }
+  int pickIndex() {
+    auto it = upper_bound(probs.begin(), probs.end(), dist(mt));
+    return distance(probs.begin(), it);
+    /*
         double pick = (rand() % 10100) / 100.0;
         int beg = 0, end = probs.size() - 1, mid = 0;
         while (beg < end) {
@@ -36,15 +37,15 @@ class Solution {
         }
         return beg;
         */
-    }
+  }
 };
 
 int main() {
-    vector<int> w = {1, 3};
-    Solution sol = Solution(w);
-    cout << sol.pickIndex() << endl;
-    cout << sol.pickIndex() << endl;
-    cout << sol.pickIndex() << endl;
-    cout << sol.pickIndex() << endl;
-    return 0;
+  vector<int> w = {1, 3};
+  Solution sol = Solution(w);
+  cout << sol.pickIndex() << endl;
+  cout << sol.pickIndex() << endl;
+  cout << sol.pickIndex() << endl;
+  cout << sol.pickIndex() << endl;
+  return 0;
 }
