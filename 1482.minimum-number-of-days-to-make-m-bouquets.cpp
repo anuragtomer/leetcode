38f428a8d5e3e02c561ab21719080585234607c9
Anurag Tomer <anuragtomer12@gmail.com>
2022-09-03 20:22:38 +0530

    Questions 2022/09/03

--- 1482.minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482.minimum-number-of-days-to-make-m-bouquets.cpp
@@ -5,48 +5,29 @@
 using namespace std;
 
 class Solution {
-  int loop_through(const vector<int> &bloomDay, const int &m, const int &k,
-                   const int &mid) {
-    int count = 0, bou = 0;
-    for (int i = 0; i < bloomDay.size(); ++i) {
-      if (bloomDay[i] <= mid) {
-        ++count;
-        if (count == k) {
-          count = 0;
-          ++bou;
-          if (bou == m) {
-            return bou;
-          }
-        }
-      } else {
-        count = 0;
-      }
-    }
-    return bou;
-  }
-  int binary_search(const vector<int> &bloomDay, const int &m, const int &k,
-                    int &low, int &high) {
-    int result = high;
-    while (low <= high) {
-      int mid = low + (high - low) / 2;
-      int bou = loop_through(bloomDay, m, k, mid);
-      if (bou == m) {
-        result = mid;
-        high = mid - 1;
-      } else
-        low = mid + 1;
-    }
-    return result;
-  }
 
  public:
-  int minDays(vector<int> &bloomDay, int m, int k) {
-    int n = bloomDay.size();
-    if (n < m * k)
+  int minDays(vector<int> &bloomDays, int m, int k) {
+    int n = bloomDays.size(), left = 1, right = 1e9;
+    if (m * k > n)
       return -1;
-    int low = *min_element(bloomDay.begin(), bloomDay.end());
-    int high = *max_element(bloomDay.begin(), bloomDay.end());
-    return binary_search(bloomDay, m, k, low, high);
+    while (left < right) {
+      int mid = (left + right) / 2, flow = 0, bouq = 0;
+      for (int j = 0; j < n; ++j) {
+        if (bloomDays[j] > mid) {
+          flow = 0;
+        } else if (++flow >= k) {
+          bouq++;
+          flow = 0;
+        }
+      }
+      if (bouq < m) {
+        left = mid + 1;
+      } else {
+        right = mid;
+      }
+    }
+    return left;
   }
 };
 
