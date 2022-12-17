c1b45706aa158260f456865ca0750c955f6cc27e
Anurag Tomer <anuragtomer12@gmail.com>
2022-12-17 22:27:38 +0530

    lowest common ancestor

---
+++ rust/src/lowest_common_ancestor_of_a_binary_search_tree.rs
@@ -0,0 +1,48 @@
+// Definition for a binary tree node.
+use std::cell::RefCell;
+use std::rc::Rc;
+
+use crate::util::tree::TreeNode;
+
+impl Solution {
+    pub fn lowest_common_ancestor(
+        mut root: Option<Rc<RefCell<TreeNode>>>,
+        p: Option<Rc<RefCell<TreeNode>>>,
+        q: Option<Rc<RefCell<TreeNode>>>,
+    ) -> Option<Rc<RefCell<TreeNode>>> {
+        let (p, q) = match (p.unwrap().borrow().val, q.unwrap().borrow().val) {
+            (p, q) if p > q => (q, p),
+            t => t,
+        };
+
+        loop {
+            let expected = root.as_ref().unwrap().borrow().val;
+            if p <= expected && expected <= q {
+                return root;
+            }
+            root = match q < expected {
+                true => root.unwrap().borrow().left.clone(),
+                _ => root.unwrap().borrow().right.clone(),
+            };
+        }
+    }
+}
+pub struct Solution {}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use crate::util::tree::to_tree;
+    #[test]
+    fn test_lowest_common_ancestor() {
+        assert_eq!(
+            Solution::lowest_common_ancestor(
+                tree![6, 2, 8, 0, 4, 7, 9, null, null, 3, 5],
+                tree![2, 0, 4, null, null, 3, 5],
+                tree![8, 7, 9]
+            ),
+            tree![6, 2, 8, 0, 4, 7, 9, null, null, 3, 5]
+        );
+    }
+}
