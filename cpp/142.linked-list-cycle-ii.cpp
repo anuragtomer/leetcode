169599b3fff674b6ddc3f6fe03c2c85401475c74
Anurag Tomer <anuragtomer12@gmail.com>
2023-06-06 17:15:10 +0530

    6 Jun Leetcode

--- cpp/142.linked-list-cycle-ii.cpp
+++ cpp/142.linked-list-cycle-ii.cpp
@@ -1,10 +1,15 @@
-class Solution {
-  struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(nullptr) {}
-  };
+#include <algorithm>
+#include <iostream>
+#include <vector>
 
+using namespace std;
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
  public:
   ListNode *detectCycle(ListNode *head) {
     ListNode *slow = head, *fast = head;
@@ -25,3 +30,33 @@ class Solution {
     return nullptr;
   }
 };
+
+int main() {
+  Solution sol;
+  auto lDeleteListNode = [](ListNode *head) {
+    return;
+    // Find a clean way to free up memory.
+    while (head) {
+      auto temp = head;
+      head = head->next;
+      delete temp;
+      temp = nullptr;
+    }
+  };
+  ListNode *head = new ListNode(3);
+  head->next = new ListNode(2);
+  head->next->next = new ListNode(0);
+  head->next->next->next = new ListNode(-4);
+  head->next->next->next->next = head->next;
+  assert(head->next == sol.detectCycle(head));
+  lDeleteListNode(head);
+  head = new ListNode(1);
+  head->next = new ListNode(2);
+  head->next->next = head;
+  assert(head == sol.detectCycle(head));
+  lDeleteListNode(head);
+  head = new ListNode(1);
+  assert(nullptr == sol.detectCycle(head));
+  lDeleteListNode(head);
+  return 0;
+}
