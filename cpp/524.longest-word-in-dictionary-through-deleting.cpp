a20780375f7eac2a851e855105d3d49e667441cf
Anurag Tomer <anuragtomer12@gmail.com>
2023-06-16 09:15:04 +0530

    16 Jun Leetcode

--- cpp/524.longest-word-in-dictionary-through-deleting.cpp
+++ cpp/524.longest-word-in-dictionary-through-deleting.cpp
@@ -7,26 +7,31 @@ using namespace std;
 class Solution {
  public:
   string findLongestWord(string s, vector<string> &dictionary) {
-    sort(dictionary.begin(), dictionary.end(), [](string &a, string &b) {
-      if (a.size() == b.size())
-        return a < b;
-      return a.size() > b.size();
-    });
-    for (int i = 0; i < dictionary.size(); ++i) {
-      int j = 0;
-      for (int k = 0; j < dictionary[i].size() && k < s.size(); ++k) {
-        if (dictionary[i][j] == s[k])
-          ++j;
+    sort(dictionary.begin(), dictionary.end(),
+         [](string &existing, string &incoming) {
+           if (incoming.size() == existing.size())
+             return incoming > existing;
+           return incoming.size() < existing.size();
+         });
+    string result;
+    for (auto &word : dictionary) {
+      int i = 0;
+      for (int j = 0; i < word.size() && j < s.size(); ++j) {
+        if (word[i] == s[j])
+          ++i;
       }
-      if (j == dictionary[i].size())
-        return dictionary[i];
+      if (i == word.size()) {
+        return word;
+      }
     }
     return "";
   }
 };
-
 int main() {
   Solution sol;
-
+  vector<string> dictionary = {"ale", "apple", "monkey", "plea"};
+  assert("apple" == sol.findLongestWord("abpcplea", dictionary));
+  dictionary = {"a", "b", "c"};
+  assert("a" == sol.findLongestWord("abpcplea", dictionary));
   return 0;
 }
