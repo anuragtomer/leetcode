e66165d4a89f94218977777b2969ca15555f1c77
Anurag Tomer <anuragtomer12@gmail.com>
2023-06-16 10:04:45 +0530

    16 Jun Leetcode

--- cpp/2131.longest-palindrome-by-concatenating-two-letter-words.cpp
+++ cpp/2131.longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -1,27 +1,49 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
 class Solution {
  public:
   int longestPalindrome(vector<string> &words) {
-    unordered_map<string, int> seen;
-    int len = 0;
+    unordered_map<string, int> rev_words;
+    int result = 0;
+    string palin_word;
     for (auto &word : words) {
-      if (seen.find(word) != seen.end() && seen[word] > 0) {
-        len += 4;
-        seen[word]--;
+      if (rev_words[word] > 0) {
+        result += 2 * word.size();
+        --rev_words[word];
       } else {
-        string copy = word;
-        reverse(copy.begin(), copy.end());
-        seen[copy]++;
+        auto rev = word;
+        reverse(rev.begin(), rev.end());
+        ++rev_words[rev];
       }
     }
-    for (auto it : seen) {
-      if (it.second > 0) {
-        string copy = it.first;
-        reverse(copy.begin(), copy.end());
-        if (copy == it.first) {
-          return len + 2;
-        }
+    size_t max_palin_word_size = 0;
+    for (auto [word, count] : rev_words) {
+      if (count == 0)
+        continue;
+      auto rev = word;
+      reverse(rev.begin(), rev.end());
+      if (rev == word) {
+        max_palin_word_size = max(max_palin_word_size, word.size());
       }
     }
-    return len;
+    return result + max_palin_word_size;
   }
 };
+
+int main() {
+  Solution sol;
+  vector<string> words = {"lc", "cl", "gg"};
+  assert(sol.longestPalindrome(words) == 6);
+  words = {"ab", "ty", "yt", "lc", "cl", "ab"};
+  assert(sol.longestPalindrome(words) == 8);
+  words = {"cc", "ll", "xx"};
+  assert(sol.longestPalindrome(words) == 2);
+  words = {"qo", "fo", "fq", "qf", "fo", "ff", "qq", "qf",
+           "of", "of", "oo", "of", "of", "qf", "qf", "of"};
+  assert(sol.longestPalindrome(words) == 14);
+  return 0;
+}
