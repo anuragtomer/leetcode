3595489a23c25495ade2b8a0b220123f2e22a22d
Anurag Tomer <anuragtomer12@gmail.com>
2023-06-21 23:48:16 +0530

    21 Jun Leetcode

--- cpp/721.accounts-merge.cpp
+++ cpp/721.accounts-merge.cpp
@@ -46,7 +46,49 @@ class Solution {
   }
 };
 int main() {
+  auto lMatch = [](vector<vector<string>> out,
+                   vector<vector<string>> &exp) -> bool {
+    if (out.size() != exp.size())
+      return false;
+    sort(out.begin(), out.end());
+    sort(exp.begin(), exp.end());
+    for (int i = 0; i < out.size(); ++i) {
+      if (out[i].size() != exp[i].size())
+        return false;
+      if (out[i][0] != exp[i][0])
+        return false;
+      sort(out[i].begin() + 1, out[i].end());
+      sort(exp[i].begin() + 1, exp[i].end());
+      for (int j = 1; j < out[i].size(); ++j)
+        if (out[i][j] != exp[i][j])
+          return false;
+    }
+    return true;
+  };
   Solution sol;
+  vector<vector<string>> accounts;
+  vector<vector<string>> expected;
+  accounts = {{"John", "johnsmith@mail.com", "john_newyork@mail.com"},
+              {"John", "johnsmith@mail.com", "john00@mail.com"},
+              {"Mary", "mary@mail.com"},
+              {"John", "johnnybravo@mail.com"}};
+  expected = {
+    {"John", "johnnybravo@mail.com"},
+    {"Mary", "mary@mail.com"},
+    {"John", "john00@mail.com", "john_newyork@mail.com", "johnsmith@mail.com"}};
+  assert(lMatch(sol.accountsMerge(accounts), expected));
 
+  accounts = {{"Gabe", "Gabe0@m.co", "Gabe3@m.co", "Gabe1@m.co"},
+              {"Kevin", "Kevin3@m.co", "Kevin5@m.co", "Kevin0@m.co"},
+              {"Ethan", "Ethan5@m.co", "Ethan4@m.co", "Ethan0@m.co"},
+              {"Hanzo", "Hanzo3@m.co", "Hanzo1@m.co", "Hanzo0@m.co"},
+              {"Fern", "Fern5@m.co", "Fern1@m.co", "Fern0@m.co"}};
+  expected = {{"Fern", "Fern0@m.co", "Fern1@m.co", "Fern5@m.co"},
+              {"Hanzo", "Hanzo0@m.co", "Hanzo1@m.co", "Hanzo3@m.co"},
+              {"Ethan", "Ethan0@m.co", "Ethan4@m.co", "Ethan5@m.co"},
+              {"Kevin", "Kevin0@m.co", "Kevin3@m.co", "Kevin5@m.co"},
+              {"Gabe", "Gabe0@m.co", "Gabe1@m.co", "Gabe3@m.co"}};
+
+  assert(lMatch(sol.accountsMerge(accounts), expected));
   return 0;
 }
